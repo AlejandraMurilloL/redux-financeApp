e35bdca05b4e3eedffe40cf269b22160bbf4de71
DIGITALWARE\AlejandraML <alejandraml@digitalware.com.co>
2022-10-10 16:13:32 -0500

    Se implementan login y registro

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { getTranslatedError } from '@nafuzi/firebase-auth-error-translator';
+import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +13,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder,
+              private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  login() {
+
+    if (this.loginForm.invalid) return;
+
+    Swal.fire({
+      title: 'Espere por favor',
+      didOpen: () => {
+        Swal.showLoading()
+      }
+    });
+
+    const { email, password } = this.loginForm.value;
+    this.authService.login(email, password).then(credentials => {
+      Swal.close();
+      this.router.navigate(['/']);
+    }).catch(error => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: getTranslatedError('es', error.code),
+      })
+    });
   }
 
 }
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -11,7 +14,9 @@ export class RegisterComponent implements OnInit {
 
   registerForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,
+              private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit(): void {
     this.registerForm = this.fb.group({
@@ -22,8 +27,25 @@ export class RegisterComponent implements OnInit {
   }
 
   createUser(): void {
-    console.log(this.registerForm);
-    console.log(this.registerForm.valid);
-    console.log(this.registerForm.value);
+    if (this.registerForm.invalid) return;
+
+    Swal.fire({
+      title: 'Espere por favor',
+      didOpen: () => {
+        Swal.showLoading()
+      }
+    });
+
+    const { name, email, password } = this.registerForm.value;
+    this.authService.createUser(name, email, password).then(credenciales => {
+      Swal.close();
+      this.router.navigate(['/']);
+    }).catch(error => {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: error,
+      })
+    });
   }
 }
