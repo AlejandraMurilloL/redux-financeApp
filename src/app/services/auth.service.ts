d42ffc0155f90ad2639e1af740a747b5b19e90fb
DIGITALWARE\AlejandraML <alejandraml@digitalware.com.co>
2022-10-10 16:49:27 -0500

    Se almacena user en el firestore

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { map } from 'rxjs';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(public auth: AngularFireAuth) { }
+  constructor(public auth: AngularFireAuth,
+              public firestore: AngularFirestore) { }
 
   initAuthListener() {
     this.auth.authState.subscribe(firebaseUser => {
@@ -17,9 +20,14 @@ export class AuthService {
     });
   }
 
-  createUser(nombre: string, email: string, password: string) {
-    //console.log({nombre, email, password});
-    return this.auth.createUserWithEmailAndPassword(email, password);
+  createUser(name: string, email: string, password: string) {
+
+    return this.auth.createUserWithEmailAndPassword(email, password)
+      .then(({ user }) => {
+        const newUser = new User(user!.uid, name, email);
+        return this.firestore.doc(`${ user?.uid }/user`)
+          .set({...newUser});
+      });
   }
 
   login(email: string, password: string) {
