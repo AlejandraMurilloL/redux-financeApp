99ad25589e9729e500c4cfcc352971ca9a35dd05
DIGITALWARE\AlejandraML <alejandraml@digitalware.com.co>
2022-10-10 12:44:56 -0500

    Se crea el app routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { DetailsComponent } from './finance/details/details.component';
 import { FinanceComponent } from './finance/finance.component';
 import { StatisticsComponent } from './finance/statistics/statistics.component';
-import { DetailsComponent } from './finance/details/details.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
@@ -26,7 +27,8 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
     SidebarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
